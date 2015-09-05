e094b3406a167b23a73adf9833089c43152ca05d
Nick Lopez <nlopez@amplify.com>
2015-09-05 13:18:35 -0400

    consolidate dnsmasq conf generation

--- dnsmasq.sh
+++ dnsmasq.sh
@@ -2,9 +2,17 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-awk -F, 'NR>1 {mac=$3; ip=$2; hostname=$1} {printf "dhcp-host=%s,%s,%s\n", mac, ip, hostname}' hosts.csv > dnsmasq.conf
-echo >> dnsmasq.conf
-sed '1d' hosts.csv | awk -F, '{print "host-record=" $1 "," $1 ".radoncanyon.com," $2}' >> dnsmasq.conf
+awk -F, 'NR>1{
+  ip6prefix="2604:2000:14c4:a1ed::"
+  domain="radoncanyon.com"
+  mac=$3
+  ip=$2
+  hostname=$1
+  n=split(ip,arr,".");
+  ip6=arr[n]
+  printf "dhcp-host=%s,%s,%s,%s\n", mac, ip, ("[" ip6prefix ip6 "]"), hostname
+  printf "host-record=%s,%s,%s,%s\n", hostname, (hostname "," domain), ip, (ip6prefix ip6)
+}' hosts.csv > dnsmasq.conf
 echo 'Generated dnsmasq.conf from hosts.csv'
 
 exit 0
