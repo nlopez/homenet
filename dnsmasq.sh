f604a43e0746bd720ec9fc700c79418c607ccb13
Nick Lopez <nlopez@amplify.com>
2015-09-05 12:43:45 -0400

    begin conversion into more readable awk

--- dnsmasq.sh
+++ dnsmasq.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-sed '1d' hosts.csv | awk -F, '{print "dhcp-host=" $3 "," $2 "," $1 }' > dnsmasq.conf
+awk -F, 'NR>1 {mac=$3; ip=$2; hostname=$1} {printf "dhcp-host=%s,%s,%s\n", mac, ip, hostname}' hosts.csv > dnsmasq.conf
 echo >> dnsmasq.conf
 sed '1d' hosts.csv | awk -F, '{print "host-record=" $1 "," $1 ".radoncanyon.com," $2}' >> dnsmasq.conf
 echo 'Generated dnsmasq.conf from hosts.csv'
