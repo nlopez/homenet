a710fab8f6c19506c2ed0231502324d645f41baf
Nick Lopez <nlopez@gmail.com>
2017-01-28 19:38:06 +0000

    pep8

--- hosts.py
+++ hosts.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
 from __future__ import print_function
 from netaddr import IPAddress, EUI, AddrFormatError
-import yaml
 from textwrap import dedent
-
+import yaml
 
 with open("hosts.yml", 'r') as stream:
     try:
@@ -39,7 +38,9 @@ print(preamble)
 for host in hosts_valid:
     print(
         'set system static-host-mapping host-name %(name)s inet %(ip)s'
-        '\nset service dhcp-server shared-network-name LAN1 subnet 192.168.239.0/24 static-mapping %(name)s ip-address %(ip)s'
-        '\nset service dhcp-server shared-network-name LAN1 subnet 192.168.239.0/24 static-mapping %(name)s mac-address %(mac)s'
+        '\nset service dhcp-server shared-network-name LAN1 subnet '
+        '192.168.239.0/24 static-mapping %(name)s ip-address %(ip)s'
+        '\nset service dhcp-server shared-network-name LAN1 subnet '
+        '192.168.239.0/24 static-mapping %(name)s mac-address %(mac)s'
         % host)
 print(postamble)
